bd0135980f2357d7b346c44be5b68110b1d773fe
Piyush Tirpude <piyushtirpude@gmail.com>
2025-10-24 12:42:21 +0530

    fixed a bug in Restaurantmenu component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import User from './user'
+import UserClass from './UserClass'
 
 const About = () => {
   return (
     <div>
         <h1>This is the About Page for Mama's Kitchen</h1>
+        <UserClass name="K G Tirpude"/>
     </div>
   )
 }
